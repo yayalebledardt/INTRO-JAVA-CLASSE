68863e5e838de4f751d1b1a81fc60c5b7fc5bf81
yayalebledardt <yanispro917@gmail.com>
2026-04-07 14:20:04 +0200

    Mission 1

---
+++ src/tp/pkg2/TP2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp.pkg2;
+
+/**
+ *
+ * @author yayar
+ */
+public class TP2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String Marque ="Peugeot";
+        int Modele = 208;
+        int annee =2021;
+        int prix =  18000;
+        
+        System.out.println("La voiture "+Marque+" "+Modele+" de l'année"+annee+" au prix  de : "+prix+"euros");
+    }
+    
+}
