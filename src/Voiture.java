3fa8eedd46eab6ee671820f8775c241c8824715f
yayalebledardt <yanispro917@gmail.com>
2026-04-07 14:29:16 +0200

    Mission 3

---
+++ src/Voiture.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author yayar
+ */
+public class Voiture {
+    String marque;
+    String  modele;
+    int annee;
+    double prix;
+    
+}
